f30189e13d2879a6f96741ee9a53c39d0aaaf704
Alberto <albertoandreotti@gmail.com>
2021-02-26 13:09:30 -0300

    tests for emr

--- python/run-emr-tests.py
+++ python/run-emr-tests.py
@@ -1,17 +1,21 @@
 from test_emr.read_write_s3 import *
 from test_emr.pretrained import *
+from test_emr.basic import *
+from test_emr.tf import *
+#from test_emr.lm import *
+
 # Read/Write S3.
-unittest.TextTestRunner().run(ReadWriteS3())
+unittest.TextTestRunner().run(ReadWriteS3TestSpec())
 
 # Pretrained models(some of them).
-unittest.TextTestRunner().run(Pretrained())
+unittest.TextTestRunner().run(PretrainedTestSpec())
 
 # Basic annotators
-#unittest.TextTestRunner().run(BasicAnnotatorsTestSpec())
+unittest.TextTestRunner().run(BasicAnnotatorsTestSpec())
 
 # Tensorflow models(NER & Spell).
-#unittest.TextTestRunner().run(BasicAnnotatorsTestSpec())
+unittest.TextTestRunner().run(TensorflowTestSpec())
 
 # Language models like Bert.
-#unittest.TextTestRunner().run(BasicAnnotatorsTestSpec())
+#unittest.TextTestRunner().run(LanguageModelsTestSpec())
 
