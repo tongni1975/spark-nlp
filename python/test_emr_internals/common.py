65b20a0d4986113eca9674d9e8b3aa9febf23b3a
Alberto <albertoandreotti@gmail.com>
2021-04-02 11:07:49 -0300

    updated maven coordinates

--- python/test_emr_internals/common.py
+++ python/test_emr_internals/common.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 def get_spark():
     spark = SparkSession.builder \
         .master("yarn") \
-        .config("spark.jars", './sparknlp.jar') \
+        .config("spark.packages", 'com.johnsnowlabs.nlp:spark-nlp_2.12:3.0.1') \
         .config("spark.driver.memory", "12G") \
         .config("spark.driver.maxResultSize", "2G") \
         .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
