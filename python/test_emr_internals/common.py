cf7f3769b3756c9abb774b14e2634fec5faf8767
Alberto <albertoandreotti@gmail.com>
2021-04-02 11:41:34 -0300

    updated spark context creation

--- python/test_emr_internals/common.py
+++ python/test_emr_internals/common.py
@@ -1,13 +1,6 @@
-from pyspark.sql import SparkSession
-
+from sparknlp_jsl import start
+import os
 def get_spark():
-    spark = SparkSession.builder \
-        .master("yarn") \
-        .config("spark.packages", 'com.johnsnowlabs.nlp:spark-nlp_2.12:3.0.1') \
-        .config("spark.driver.memory", "12G") \
-        .config("spark.driver.maxResultSize", "2G") \
-        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
-        .config("spark.kryoserializer.buffer.max", "500m") \
-        .getOrCreate()
-    return spark
-
+    secret = os.environ['JSL_SECRET']
+    _params = {'master' : 'yarn'}
+    return start(secret, params=_params)
